466db85b0f693f1f0d71f243269fdb52966c123b
Michael Choe <michael@straussmade.com>
2025-12-30 20:24:57 -0800

    Remove build folder

--- Weather/build/Weather.build/Release-iphoneos/WeatherTests.build/DerivedSources/WeatherTests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char WeatherTestsVersionString[];
- extern const double WeatherTestsVersionNumber;
-
- const unsigned char WeatherTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:WeatherTests  PROJECT:Weather-1" "\n";
- const double WeatherTestsVersionNumber __attribute__ ((used)) = (double)1.;
